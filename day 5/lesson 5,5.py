ec727cfd364c82b3dae24065fb0ddfc3959d3140
shonia-davit <david.shonia11@gmail.com>
2024-03-15 15:08:39 +0400

    commit

---
+++ day 5/lesson 5,5.py
@@ -0,0 +1,9 @@
+num1 = 80
+
+num2 = 40
+
+print(num1/num2)
+
+print(type(num1))
+
+print(type(num2))
---
+++ day 5/lesson 5.py
@@ -0,0 +1,27 @@
+num1 = 13
+
+num2 = 83
+
+name1 = "luka"
+
+name2 = "daviti"
+
+num3 = 23
+
+num4 = 53
+
+name3 = "tornike"
+
+print(type(name1))
+
+print(type(num1))
+
+print(type(name2))
+
+print(type(num3))
+
+print(type(num2))
+
+print(type(name3))
+
+print(type(num4))
